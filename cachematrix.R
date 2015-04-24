7022f2621aa8213a73f3ecdbc305415b1fc1d987
Cap <Tangent999@126.com>
2015-04-24 17:15:38 -0500

    Add all comments. Correct a bug in setsolve.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions exercise on the scoping rule of R. More specifically, 
+## the inverse of a first-time input matrix is put into cache. When we need it
+## again later, it can be looked up in the cache, not being recomputed, saving
+## need to redo the costly matrix inverse computation again.
 
-## This function makes a matrix
+## This function sets the value of the matrix, gets the value of the matrix,
+## sets the value of the inverse, gets the value of the inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -10,7 +13,7 @@ makeCacheMatrix <- function(x = matrix()) {
                 m <<- NULL
         }
         get <- function() x
-        setsolve <- function(mean) m <<- solve
+        setsolve <- function(solve) m <<- solve
         getsolve <- function() m
         list(set = set, get = get,
              setsolve = setsolve,
@@ -19,7 +22,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse. It first checks whether the inverse
+## is already in the cache. If not, it will perform the calculation.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
